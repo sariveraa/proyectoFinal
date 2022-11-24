a499d8ef58dd4fe5b2aae504e9c4fc43d673b2db
Salomé Rivera <sariveraa@unal.edu.co>
2022-11-24 17:13:37 -0500

    se sube la pagina principal terminada

--- proyectoFinal/resources/views/viewadmin.blade.php
+++ proyectoFinal/resources/views/viewadmin.blade.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de inicio</title>
+    <title>Página de administrador</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body style="background-color:#FFD1E9;margin-top:0px;margin-left:0px;">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
@@ -23,7 +22,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a href="nav-link active" class="nav-link active" aria-current="page" href="#">PRINCIPAL</a>
+                        <a href="{{ route('welcome') }}" class="nav-link active" aria-current="page" href="#">PRINCIPAL</a>
                     </li>
                     <li class="nav-item">
                         <a href="{{ url('/home') }}" class="nav-link active" aria-current="page" href="#">INICIAR SESIÓN</a>
@@ -31,6 +30,9 @@
                     </li>
                     <li class="nav-item">
                         <a href="{{ route('register') }}" class="nav-link active" aria-current="page" href="#">REGISTRARSE</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('viewadmin') }}" class="nav-link active" aria-current="page" href="#">ADMIN</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">CATÁLOGO</a>
@@ -105,16 +107,12 @@
     
     <div class="row gx-5">
         <div class="col">
-        <div class="p-3"><img src="ropa6.png" class="img-fluid" alt="Responsive image" width="250"></div>
+        <div class="p-3">Custom column padding</div>
         </div>
         <div class="col">
-            <div class="p-3"><img src="ropa8.png" class="img-fluid" alt="Responsive image" width="250"></div>
+            <div class="p-3">Custom column padding</div>
         </div>
     </div>
-    <footer class="footer">
-        <h5>Siguenos en nuestras redes sociales</h5>
-        <a href="https://www.instagram.com/samtaofi/"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAXRJREFUSEvFlbsuRFEUhr9pNFPQaDRDLy4vwLwBSokErQTxBHgDxNQukSh5AzyBywugVqmU5Je15WTZ+5x9ZnIyuztnr/X/a/3rsls0fFoN4zM0gjlgF+gCkxVZvgH3wDHw5G1jGWwAZ31KtwmcF309gSJ/7BM8uM0XM/EEYl+vQfAJLJuU++Z3AUiF3+MJpGcnQSBHaTxmdVEg+lbEIrkxP2FMpQi+I+DPBiDHcbv/ACSnMh41UhH/w/UZxAgUzQSwBiwCsnkAroCvRM3+cKsIgp49YMtldwpsA7fAkrvLJtgDLi3iaQfyAswCB0AocG2JDoET4A6YcQShwAMRBIk0pTuO4AhQhgNJJEy14QiwCiwYiYp8DbQtu+SGqCqyHNWeK5E9oz2l3i+2Z2UNygZNUoRlphnQcMWO5kb30UmuuypiBKWrQqv5NRFZ7u/SZSeQRtd1iFKZqL9VyNTyC7bv9uCobbMenFwpsuyG9iZnRZdj9AO5y0cZdyMG3QAAAABJRU5ErkJggg=="/></a>
-        <a href="https://www.facebook.com/Samtaoficial"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAUxJREFUSEu1lYFNwzAQRV8nYATKBMAE7QgwAXQCYALoBLQTABMAGzAC3QA2gAlAP7Ktw7V9DlJOsiIl9r3z+ftnxsQxmzg/PYBLYAGchKGa3sN4AV5bRbYAZ8A9MHd2+QHcAILtRQ2wAa5Gtk9rBPoTJcB/ksekW+DaEnKA2vLcqPwzJLDtuANuzZpz264coH4eVgDf4ZA1x0YO0PejOMECpJaHRvVPgObEWAZ16alhI+3CAh6BiwZgDahahdfKVIwFSNvHnYC8Lfky5TrVSwv4cWRpd+ABUu4egHfb38JZ5PUN6+zimoI8wBdwkGXfRVvpOWQPUGpt8ZA9mY45gxUgVe65aeui9QJ025NBjrGKXkDTKrSrmtn1AFyzi2Io3WoPkFvJkMv74Wg30fxqgJLDJtV6EtREqUtmpgs1KKPyrmgEPQDHQdqfJwf8ArJlSRlOmzLXAAAAAElFTkSuQmCC"/></a>
-    </footer>
+
 </body>
 </html>

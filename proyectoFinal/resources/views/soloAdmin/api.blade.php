25a65dab7f8b47c5e028866a553c37e88d1215a8
lauraqc <lquinterocu@unal.edu.co>
2022-11-29 18:31:33 -0500

    Inicio de la api

--- proyectoFinal/resources/views/soloAdmin/api.blade.php
+++ proyectoFinal/resources/views/soloAdmin/api.blade.php
@@ -10,7 +10,63 @@ Usuario
 
 
 <script>
-    fetch("{{ url(/api/datosApi) }}")
+    var contenido = document.getElementById("contenido")
+
+    fetch('http://localhost:8000/api/datosApi')
+        .then(datos => datos.json())
+        .then(datos => {
+            console.log(datos)
+            tabla(datos)
+        })
+
+    function tabla(datos) {
+
+        for (let valor of datos) {
+            console.log(valor.nombre)
+            contenido.innerHTML += `
+            <tr>
+                <th scope="row">${valor.id}</th>
+                <td>${valor.identidad}</td>
+                <td>${valor.nombre}</td>
+                <td>${valor.direccion}</td>
+                <td>${valor.codigo}</td>
+              </tr>
+        `
+        }
+    }
 </script>
+
+
+
+<body>
+
+
+    <div></div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Identidad</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Direccion</th>
+                <th scope="col">codigo</th>
+            </tr>
+        </thead>
+        <tbody id="contenido">
+
+        </tbody>
+    </table>
+
+
+    <script>
+        
+    </script>
+</body>
+
+</html>
+
+
+
+
 
 @endsection
